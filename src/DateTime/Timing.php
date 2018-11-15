c7723269f72f7fe8db206e5bc4ab8c0cbf99f7aa
Shane Perera <shane@shaneperera.com>
2018-11-15 23:33:17 +1100

    Remove unwanted function

--- src/DateTime/Timing.php
+++ src/DateTime/Timing.php
@@ -35,23 +35,7 @@ class Timing
 		return date('Y-m-d H:i:s', strtotime(trim($string)));
 	}
 
-	/**
-	 *
-	 * Convert a UTC time-string to the applications timezone.
-	 * Useful to convert JavaScript date strings to Carbon dates.
-	 *
-	 * @param $UTCTimeString
-	 *
-	 * @return Carbon
-	 */
-	public static function toServerTimezone($UTCTimeString, $onlyDate = false)
-	{
-		// convert the UTC date sent by client to our format
-		$reportDate = new \Carbon\Carbon($UTCTimeString);
-		$reportDate->setTimezone(config('app.timezone', 'UTC'));
-		if ($onlyDate) $reportDate->startOfDay();
-		return $reportDate;
-	}
+
 
 	public static function toUTCTimezone($serverTimeString)
 	{
