34216ca6f53fddde8c0b3d261059d8bc264140b2
Shane Perera <shane@shaneperera.com>
2020-05-01 16:47:57 +1000

    Add a more meaningful error to check_all_present

--- src/global_functions/validation_helpers.php
+++ src/global_functions/validation_helpers.php
@@ -3,7 +3,9 @@
 
 use EMedia\PHPHelpers\Util\Check;
 
-if (!function_exists('check_all_present')) {
+if (!function_exists('check_all_present'))
+{
+
 	/**
 	 *
 	 * Check all values are !empty
@@ -15,7 +17,27 @@ if (!function_exists('check_all_present')) {
 		$args = func_get_args();
 
 		if (!Check::all_present($args)) {
-			throw new InvalidArgumentException('At least one required variable is not present');
+			$errorMessage = 'At least one required variable is not present.';
+
+			// show the variable location on the debug mode
+			if (getenv('APP_DEBUG') === 'true') {
+
+				// show the argument location for easier debugging
+				$backtrace = debug_backtrace();
+
+				if (isset($backtrace[0])) {
+					$args = $backtrace[0]['args'];
+
+					foreach ($args as $i => $iValue) {
+						if ($iValue === null) {
+							$errorMessage .= ' Argument ' . ($i + 1) . ' is empty.';
+						}
+					}
+				}
+			}
+
+			throw new InvalidArgumentException($errorMessage);
 		}
 	}
+
 }
